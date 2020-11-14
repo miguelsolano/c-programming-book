8e1584addac49651b0634c89fea487945bed00ee
miguelsolano <miguel.solano@hey.com>
2020-11-14 12:27:22 -0600

    [feat] Initial Commit chapter 1

---
+++ cmake-build-debug/CMakeFiles/c_programming_book.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_programming_book.dir/main.c.o"
+  "c_programming_book"
+  "c_programming_book.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_programming_book.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
